e1e9d316648e576b93fb0bbb5c0cc10e4c1ed4c5
ArMp77 <melpiz9312@gmail.com>
2026-08-05 16:10:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ Técnico: Alfredo Meléndez
 `;
 
 async function llamarGeminiREST(contentsPayload) {
-  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${GEMINI_API_KEY}`;
+  const url = `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`;
 
   const response = await fetch(url, {
     method: 'POST',
